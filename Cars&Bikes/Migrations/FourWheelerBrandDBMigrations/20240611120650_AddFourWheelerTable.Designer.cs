fe7da69c90c16de2c2101da90ae2791f0f4b478d
shivani <shivanigusain2251@gmail.com>
2024-06-11 18:56:52 +0530

    For Test

--- Cars&Bikes/Migrations/FourWheelerBrandDBMigrations/20240611120650_AddFourWheelerTable.Designer.cs
+++ Cars&Bikes/Migrations/FourWheelerBrandDBMigrations/20240611120650_AddFourWheelerTable.Designer.cs
@@ -3,6 +3,7 @@ using Cars_Bikes.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cars_Bikes.Migrations.FourWheelerBrandDBMigrations
 {
     [DbContext(typeof(FourWheelerBrandDB))]
-    partial class FourWheelerBrandDBModelSnapshot : ModelSnapshot
+    [Migration("20240611120650_AddFourWheelerTable")]
+    partial class AddFourWheelerTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
