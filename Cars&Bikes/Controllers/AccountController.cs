7012f71a5867035c916386a719124bf53d89576e
Shivani Gusain <shivanigusain2251@gmail.com>
2025-02-03 14:02:09 +0530

    Add Meta tag in layout

--- Cars&Bikes/Controllers/AccountController.cs
+++ Cars&Bikes/Controllers/AccountController.cs
@@ -1,19 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cars_Bikes.Data;
+using Google.Apis.Drive.v3.Data;
+using Microsoft.AspNetCore.Mvc;
+using Cars_Bikes.Models;
 
 namespace Cars_Bikes.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private readonly TwoWheelerDB _context;
+
+        public AccountController(TwoWheelerDB context)
         {
+            _context = context;
+        }
+            [HttpPost]
+            public IActionResult Login(string username)
+            {
+                if (!string.IsNullOrEmpty(username)) // Just checking if username is provided
+                {
+                    HttpContext.Session.SetString("Username", username);
+                    return RedirectToAction("Dashboard", "Home"); // Redirect to a different page
+                }
+
+                ViewBag.Error = "Please enter a username!";
+                return View();
+            }
+
+            public IActionResult Logout()
+            {
+                HttpContext.Session.Clear(); // Clear session when user logs out
+                return RedirectToAction("Login");
+            }
+        public IActionResult Dashboard()
+        {
+            if (HttpContext.Session.GetString("Username") == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
             return View();
         }
-        private static readonly Dictionary<string, string> Users = new Dictionary<string, string>
-        {
-            { "user1", "password123" }
-        };
-
-        // Login Action
         //[HttpPost]
         //public ActionResult Login(string username, string password)
         //{
