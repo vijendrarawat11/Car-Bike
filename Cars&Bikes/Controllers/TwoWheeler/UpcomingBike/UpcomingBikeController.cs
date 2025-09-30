736d0968ec6e0ccb03f77a42872a2720db19ae87
shivani <shivanigusain2251@gmail.com>
2025-09-30 18:13:51 +0530

    Add upcoming pages 30 Sep

--- Cars&Bikes/Controllers/TwoWheeler/UpcomingBike/UpcomingBikeController.cs
+++ Cars&Bikes/Controllers/TwoWheeler/UpcomingBike/UpcomingBikeController.cs
@@ -296,6 +296,16 @@ namespace Cars_Bikes.Controllers.TwoWheeler.UpcomingBike
             }
             return View("~/Views/TwoWheeler/UpcomingBike/2025/Oct2025UpcomingBikeDetails10.cshtml", upcomingDetails);
         }
+        [Route("upcoming-bike/ola-diamondhead")]
+        public IActionResult Oct2025UpcomingBikeDetails11()
+        {
+            var upcomingDetails = GetUpcomingBikeDetails("OLA Diamondhead");
+            if (upcomingDetails == null)
+            {
+                return NotFound(); // Return 404 if news item is not found
+            }
+            return View("~/Views/TwoWheeler/UpcomingBike/2025/Oct2025UpcomingBikeDetails11.cshtml", upcomingDetails);
+        }
         private Cars_Bikes.Models.UpcomingBike GetUpcomingBikeDetails(string upcomingBikeName)
         {
             var upcomingItem = _context.UpcomingBikes
