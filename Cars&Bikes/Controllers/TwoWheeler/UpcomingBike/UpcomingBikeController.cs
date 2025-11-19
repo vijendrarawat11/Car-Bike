66e65e1804cf2ed4932d9a18507ac2f133c9cede
Sachin Bhardwaj <scbhardwaj43@gmail.com>
2025-11-19 15:23:01 +0530

    added 2 upcoming bikes

--- Cars&Bikes/Controllers/TwoWheeler/UpcomingBike/UpcomingBikeController.cs
+++ Cars&Bikes/Controllers/TwoWheeler/UpcomingBike/UpcomingBikeController.cs
@@ -486,6 +486,26 @@ namespace Cars_Bikes.Controllers.TwoWheeler.UpcomingBike
             }
             return View("~/Views/TwoWheeler/UpcomingBike/2025/Dec2025UpcomingBikeDetails11.cshtml", upcomingDetails);
         }
+        [Route("upcoming-bike/honda-cBR500R")]
+        public IActionResult Dec2025UpcomingBikeDetails12()
+        {
+            var upcomingDetails = GetUpcomingBikeDetails("Honda CBR500R");
+            if (upcomingDetails == null)
+            {
+                return NotFound(); // Return 404 if news item is not found
+            }
+            return View("~/Views/TwoWheeler/UpcomingBike/2025/Dec2025UpcomingBikeDetails12.cshtml", upcomingDetails);
+        }
+        [Route("upcoming-bike/indian-fTR-1200")]
+        public IActionResult Dec2025UpcomingBikeDetails13()
+        {
+            var upcomingDetails = GetUpcomingBikeDetails("Indian FTR 1200");
+            if (upcomingDetails == null)
+            {
+                return NotFound(); // Return 404 if news item is not found
+            }
+            return View("~/Views/TwoWheeler/UpcomingBike/2025/Dec2025UpcomingBikeDetails13.cshtml", upcomingDetails);
+        }
         private Cars_Bikes.Models.UpcomingBike GetUpcomingBikeDetails(string upcomingBikeName)
         {
             var upcomingItem = _context.UpcomingBikes
