da1986038571ffe06e0f93e1eff707f4ed3f1f05
shivani <shivanigusain2251@gmail.com>
2025-12-02 16:33:29 +0530

    Add new database

--- Cars&Bikes/Migrations/20251201095604_CompareItems.Designer.cs
+++ Cars&Bikes/Migrations/20251201095604_CompareItems.Designer.cs
@@ -4,6 +4,7 @@ using Cars_Bikes.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cars_Bikes.Migrations
 {
     [DbContext(typeof(TwoWheelerDB))]
-    partial class TwoWheelerDBModelSnapshot : ModelSnapshot
+    [Migration("20251201095604_CompareItems")]
+    partial class CompareItems
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
