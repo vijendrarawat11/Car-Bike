f0e87c88ceb300195d7fc354a56c9f8192e1b989
Sachin Bhardwaj <scbhardwaj43@gmail.com>
2025-12-05 17:34:48 +0530

    added 1 blog

--- Cars&Bikes/Controllers/BlogsController.cs
+++ Cars&Bikes/Controllers/BlogsController.cs
@@ -1377,6 +1377,16 @@ namespace Cars_Bikes.Controllers
             }
             return View("~/Views/Blogs/2025/Nov102025BlogDetails2.cshtml", blogsDetails);
         }
+        [Route("blogs/tata-sierra-2025")]
+        public IActionResult Nov102025BlogDetails3()
+        {
+            var blogsDetails = GetBlogDetails("Tata Sierra: The Return of a Legend That Dares to Escape Mediocrity");
+            if (blogsDetails == null)
+            {
+                return NotFound(); // Return 404 if news item is not found
+            }
+            return View("~/Views/Blogs/2025/Nov102025BlogDetails3.cshtml", blogsDetails);
+        }
 
         private Cars_Bikes.Models.Blogs GetBlogDetails(string blogsHeading)
         {
