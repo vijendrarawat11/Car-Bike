630383304a28dc9e87b84040452d311c3ba92d1c
Sachin Bhardwaj <scbhardwaj43@gmail.com>
2026-01-20 10:52:38 +0530

    added 1 blog

--- Cars&Bikes/Controllers/BlogsController.cs
+++ Cars&Bikes/Controllers/BlogsController.cs
@@ -1437,6 +1437,16 @@ namespace Cars_Bikes.Controllers
             }
             return View("~/Views/Blogs/2026/Jan012026BlogDetails3.cshtml", blogsDetails);
         }
+        [Route("blogs/all-new-tata-punch-facelift")]
+        public IActionResult Jan012026BlogDetails4()
+        {
+            var blogsDetails = GetBlogDetails("All-New Tata Punch Facelift 2025 – Price, Variants, Features, Specifications & Complete Buyer’s Guide");
+            if (blogsDetails == null)
+            {
+                return NotFound(); // Return 404 if news item is not found
+            }
+            return View("~/Views/Blogs/2026/Jan012026BlogDetails4.cshtml", blogsDetails);
+        }
         private Cars_Bikes.Models.Blogs GetBlogDetails(string blogsHeading)
         {
             var blogsItem = _context.Blogs
